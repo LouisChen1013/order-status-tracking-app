37d28bac6170f19b9289b4db4ae803f6e717923f
louis <chenhonglin1013@gmail.com>
2020-11-21 10:42:40 +0800

    updated

--- receiver/app.py
+++ receiver/app.py
@@ -77,7 +77,8 @@ def payment(body):
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path="/receiver",
+            strict_validation=True, validate_responses=True)
 
 
 if __name__ == "__main__":
